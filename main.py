5d48bc2826cc83b1c6a0e770ae87693c916bf4bc
Sean <seanreilly1@MacBook-Pro-37.local>
2022-07-05 15:44:37 -0700

    Updated to process input from command line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Author: Sean Reilly
-# Date: 1/14/2022
+# Date: 7/05/2022
 '''A program to help the user keep track of the number of words they write, 
    allowing them to add, delete, and print entries in a postgres database
    that contain wordcounts, the date those words were written and, optionally,
@@ -8,91 +8,30 @@
 '''
 import datetime
 from data_module import DataModule
+import argparse
 
-def print_commands():
-    '''Prints the commands available to the user of this program'''
-    
-    print("The following is a list of commands. [] indicates an optional "
-          "part of a command. Anything in () should not be entered directly, "
-          "but is instead a description of what sorts of values should be " 
-          "entered or what the command does:\n\n"        
-          "     help\n\n"
-          "     add (positive integer representing wordcount) "
-          "[(date in ISO format)] [(string representing the title of "
-          "a project)]\n\n"
-          "     delete (integer representing id of project, id's can be found "
-          "with the print command)\n"
-          "     delete dates (first date in range entries will be deleted in) " 
-          "(second date in range entries will be deleted in)\n"
-          "     delete project (name of project whose entries will be " 
-          "deleted)\n\n"
-          "     print all \n"
-          "     print dates (first date in range entries will be printed in) " 
-          "(second date in range entries will be printed in)\n"
-          "     print project (name of project whose entries will be printed)\n"
-          "\n     quit \n")
-
-def parse_add_command(data_module, user_input):
-    '''Parses user input for the add command
-    
-    The user can add an entry to the database by specifying a word count (in 
-    this case current date is used), a word count and date, or a word count, 
-    date, and project title. This function parses user input so that the
-    correct one of these options can be fulfilled depending on user input.
-
-    Args:
-        user_input:
-            A length 2 list of strings  representing user input from the
-            command line, with the 0th index containing the string "add"
-            and the 1st containing the rest of the command
-    '''
-    user_input = (" ".join(user_input)).split(None, 3)
-
-    if len(user_input) == 1:
-        print("Invalid command, adding an entry requires a wordcount")
-    if len(user_input) == 2:
-        data_module.add_entry(user_input[1], datetime.datetime.today(), None)
-    if len(user_input) == 3:
-        data_module.add_entry(user_input[1], user_input[2], None)
-    if len(user_input) == 4:
-        data_module.add_entry(user_input[1], user_input[2], user_input[3])
-
-def parse_delete_command(data_module, user_input):
-    '''Parses user input for the delete command
+def process_delete_command(data_module, cl_args):
+    '''Calls appropriate data module function depending on command line input
     
     The user can delete all entries in the database, an entry with a specific
     id, entries that fall within a range of dates, or all entries associated
-    with a specific project. This function function parses user input so that 
-    the correct one of these options can be fulfilled depending on user input
+    with a specific project.
 
     Args:
-        user_input:
-            A length 2 list of strings  representing user input from the
-            command line, with the 0th index containing the string "delete"
-            and the 1st containing the rest of the command
+        data_module:
+            Handles querying posgreSQL database
+        cl_args:
+            Contains parsed command line arguments
     '''
-    user_input = (" ".join(user_input)).split(None, 2)
-
-    if len(user_input) == 1:
-            print("Invalid command, specify which entries to remove")
-    elif user_input[1] == "all":
-        data_module.delete_all_entries()
-    elif len(user_input) == 2:
-        data_module.delete_entry(user_input[1])
-    elif user_input[1] == "project":
-        data_module.delete_project_entries(user_input[2])
-    elif user_input[1] == "dates":
-        # Resplit user input so dates can be processed
-        user_input = (" ".join(user_input)).split(None, 3) 
-        
-        if len(user_input) < 4:
-            print("This command requires two dates")
-        else:
-            data_module.delete_entries_in_date_range(user_input[2], 
-                                                     user_input[3])
+    if cl_args.id is not None:
+            data_module.delete_entry(cl_args.id)
+    elif cl_args.dates is not None:
+        dates = cl_args.dates
+        data_module.delete_entries_in_date_range(dates[0], dates[1])
+    elif cl_args.project is not None:
+        data_module.delete_project_entries(cl_args.project)
     else:
-        print("Invalid command, \"{}\" is not a valid way of specifying " 
-              "which entries to remove".format(user_input[1]))
+        data_module.delete_all_entries()
 
 def print_entries (entries):
     if isinstance(entries, list):
@@ -100,62 +39,62 @@ def print_entries (entries):
             print ("Entry #:", row[0], " | Wordcount:", row[1], " | Date:", 
                     row[2], " | Project Title:", row[3]) 
 
-def parse_print_command(data_module, user_input):
-    '''Parses user input for the print command
+def process_print_command(data_module, cl_args):
+    '''Calls appropriate data module function depending on command line input
     
     The user can print all entries in the database, entries that fall within a 
-    range of dates, or all entries associated with a specific project. This 
-    function function parses user input so that the correct one of these 
-    options can be fulfilled depending on user input
+    range of dates, or all entries associated with a specific project. 
 
     Args:
-        user_input:
-            A length 2 list of strings  representing user input from the
-            command line, with the 0th index containing the string "print"
-            and the 1st containing the rest of the command
+        data_module:
+            Handles querying posgreSQL database
+        cl_args:
+            Contains parsed command line arguments
     '''
-    user_input = (" ".join(user_input)).split(None, 2)
-
-    if len(user_input) == 1:
-        print("Invalid command, specify which entries to print")
-    elif user_input[1] == "all":
-        print_entries(data_module.select_all_entries())
-    elif user_input[1] == "project":
-        print_entries(data_module.select_project_entries(user_input[2]))
-    elif user_input[1] == "dates":
-        user_input = (" ".join(user_input)).split(None, 3) 
-        if len(user_input) < 4:
-            print("This command requires two dates")
-        else:
-            print_entries(data_module.select_entries_in_date_range 
-                          (user_input[2], user_input[3]))
+    if cl_args.project is not None:
+        print_entries(data_module.select_project_entries(cl_args.project))
+    elif cl_args.dates is not None:
+        dates = cl_args.dates
+        print_entries(data_module.select_entries_in_date_range 
+                        (dates[0], dates[1]))
     else:
-        print("Invalid command, \"{}\" is not a valid way of specifying " 
-              "which entries to print".format(user_input[1]))
+        print_entries(data_module.select_all_entries())
 
 def main ():
-    '''Takes user input until the user inputs "quit"'''
+    '''Proccesses command-line arguments, calling appropriate helper functions
+       for different commands
+    '''
     db = DataModule()
 
-    print("Enter help for a list of commands")
-    end_program = False 
-    while not end_program:
-        user_input = input("Command: ").split(None, 1)
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='sub-command help', dest="command")
+    subparsers.required = True
+    
+    add_parser = subparsers.add_parser('add', help='Add a wordcount entry to the database')
+    add_parser.add_argument('--wordcount', type=int, required=True, help='The wordcount of the entry to be added')
+    add_parser.add_argument("--date", default=datetime.datetime.today(),  help='The date of the entry to be added')
+    add_parser.add_argument('--project', help='The name of the project that\'s associated with the entry')
 
-        if len(user_input) < 1:
-            continue
-        if user_input[0] == "help":
-            print_commands()
-        elif user_input[0] == "add":
-           parse_add_command(db, user_input)
-        elif user_input[0] == "delete":
-            parse_delete_command(db, user_input)
-        elif user_input[0] == "print":
-            parse_print_command(db, user_input)
-        elif user_input[0] == "quit":
-            end_program = True
-        else:
-            print("Invalid command, enter help for a list of commands")
+    print_parser = subparsers.add_parser('print', help='Prints entries in the database')
+    print_group = print_parser.add_mutually_exclusive_group(required=True)
+    print_group.add_argument("--all", action='store_true', help="Prints all entries")
+    print_group.add_argument("--dates", nargs=2, help="Prints all entries between two dates")
+    print_group.add_argument('--project', help='Prints all entries associated with a project')
+
+    delete_parser = subparsers.add_parser('delete', help='Prints entries in the database')
+    delete_group = delete_parser.add_mutually_exclusive_group(required=True)
+    delete_group.add_argument("--id", type=int, help="Deletes the entry with the specified id")
+    delete_group.add_argument("--all", action='store_true', help="Deletes all entries")
+    delete_group.add_argument("--dates", nargs=2, help="Deletes all entries between two dates")
+    delete_group.add_argument('--project', help='Deletes all entries associated with a project')
+
+    args = parser.parse_args()
+    if args.command == "add":
+        db.add_entry(args.wordcount, args.date, args.project)
+    elif args.command == "print":
+        process_print_command(db, args)
+    elif args.command == "delete":
+        process_delete_command(db, args)
 
 if __name__ == "__main__":
     main()
